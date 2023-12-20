5736084b2833aa0d4d15cf9786a2a556f871b8b0
tran khanh bao <tranbao22051999@gmail.com>
2023-12-20 18:09:30 +0700

    xong video #104 sau

--- src/components/User/Content/CountDown.js
+++ src/components/User/Content/CountDown.js
@@ -1,7 +1,19 @@
 import { useEffect, useState } from 'react';
 
 const CountDown = (props) => {
-    const [count, setCount] = useState(10);
+    const [count, setCount] = useState(300);
+
+    const toHHMMSS = (secs) => {
+        const sec_num = parseInt(secs, 10);
+        const hours = Math.floor(sec_num / 3600);
+        const minutes = Math.floor(sec_num / 60) % 60;
+        const seconds = sec_num % 60;
+
+        return [hours, minutes, seconds]
+            .map((v) => (v < 10 ? '0' + v : v))
+            .filter((v, i) => v !== '00' || i > 0)
+            .join(':');
+    };
 
     useEffect(() => {
         if (count === 0) {
@@ -17,7 +29,7 @@ const CountDown = (props) => {
         };
     }, [count]);
 
-    return <div className="countdown-container">{count}</div>;
+    return <div className="countdown-container">{toHHMMSS(count)}</div>;
 };
 
 export default CountDown;
